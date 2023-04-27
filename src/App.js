1a62c7a5af35dd7eed284bfd9cf735c289e7923a
AndriiKostenko <kostenkoandrii.dev@gmail.com>
2023-04-27 19:05:44 -0400

    3 Search boxes added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Button from "./components/Button";
+import Search1 from "./components/search/Search1";
+import Search2 from "./components/search/Search2";
+import Search3 from "./components/search/Search3";
 
 function App() {
   return (
@@ -9,6 +12,11 @@ function App() {
         <Button className="bg-green-600">Button</Button>
         <Button className="bg-purple-600">Button</Button>
         <Button className="bg-cyan-600">Button</Button>
+      </div>
+      <div className="flex gap-x-4">
+        <Search1 />
+        <Search2 />
+        <Search3 />
       </div>
     </div>
   );
