78eaaa4c8db9e6d23b6fdb7caa2bde7265456e9f
AndriiKostenko <kostenkoandrii.dev@gmail.com>
2023-04-27 19:20:16 -0400

    2 Registrations added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import Login1 from './components/auth/Login1';
-import Login2 from './components/auth/Login2';
-import Login3 from './components/auth/Login3';
+import Login1 from './components/Login/Login1';
+import Login2 from './components/Login/Login2';
+import Login3 from './components/Login/Login3';
 
 import Accordion1 from './components/accordion/Accordion1';
 import Accordion2 from './components/accordion/Accordion2';
@@ -30,12 +30,15 @@ import Navbar2 from './components/navbar/Navbar2';
 import Modal1 from './components/dialog/Modal1';
 import Modal3 from './components/dialog/Modal3';
 
+import Registration1 from './components/registration/Registration1';
+import Registration2 from './components/registration/Registration2';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Modal3 />
+      <Registration2 />
   </React.StrictMode>
 );
 
